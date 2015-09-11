7ffba96a8653babebab89d6817c8aa572fd4b8d5
Alex Mansfield <alex@sackclothstudios.com>
2015-09-11 12:16:59 -0700

    Grunt jscs improvements.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,10 +34,8 @@ module.exports = function(grunt) {
 		jscs: {
 		    src: ['Gruntfile.js', 'js/*.js'],
 		    options: {
-		        config: ".jscsrc",
-		        esnext: true, // If you use ES6 http://jscs.info/overview.html#esnext
-		        verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
-		        requireCurlyBraces: [ "if" ]
+		        // verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
+		        preset: "wordpress"
 		    }
 		},
 
